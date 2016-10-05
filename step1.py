7a4d9eb9314de2f59da47fcb99aa14cbd2f27922
rgpm <rogomezpalacio@gmail.com>
2016-10-05 14:02:34 -0600

    step1

---
+++ step1.py
@@ -0,0 +1,3 @@
+import requests
+
+r=requests.post('http://challenge.code2040.org/api/register', data = {'token':'06da63edb778ccce75da5f89aaffc09e','github':'https://github.com/rgomezp/code2040app'})
